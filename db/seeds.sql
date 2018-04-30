6e54e4aaa7129aa3efc5b720a5099b1315acdad7
Monti <michaelmonti@hotmail.com>
2018-04-30 15:58:35 -0500

    files and folders

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Regular Hamburger on Bun");
+INSERT INTO burgers (burger_name) VALUES ("Royale Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("The Boss");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Double Cheeseburger");
+
+SELECT * FROM burgers;
